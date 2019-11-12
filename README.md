3170cb5b09a7d900b494fa97e07fa5d164a4dda4
IcecreamVille <dandevoe@gmail.com>
2019-11-12 20:24:20 +0000

    My first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Project Title
+
+In this space, I will create a new application
+for handling IT service requests
---
+++ list.js
@@ -0,0 +1,17 @@
+
+function list() {
+  pjs.defineDisplay("display", "list.json");
+
+  while (true) {
+    var records = pjs.query("SELECT * FROM requests");
+    display.grid.replaceRecords(records);
+    display.list.execute();
+
+    display.grid.readChanged();
+    if (!pjs.endOfData()) {
+      pjs.query("UPDATE requests SET ? WHERE ID = ?", [{ complete }, id]);
+    }
+  }
+}
+
+exports.default = list;
